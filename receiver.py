73342ba9d06c23a88d50b74607b29f5b44533dfa
Danny Taing <dantaing@gmail.com>
2014-11-09 15:33:19 -0500

    trying to implement checksum

--- receiver.py
+++ receiver.py
@@ -10,14 +10,11 @@ import datetime
 
 BUFFSIZE = 1024
 MSS = 576
+logFile = ""
+HEADER_LEN = 20
 
 def main():
-    # Assign command line args to variables
-    filename = sys.argv[1]
-    listeningPort = int(sys.argv[2])
-    senderIP = sys.argv[3]
-    senderPort = int(sys.argv[4])
-    logFilename = sys.argv[5]
+    filename, listeningPort, senderIP, senderPort, logFilename = getArgs()
 
     dsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     dsock.bind(("", listeningPort))
@@ -28,6 +25,8 @@ def main():
 
     tcpConnected = False
     outfile = open(filename, "wb")
+    global logFile
+    logFile = open(logFilename, "w")
     i = 0
     while 1:
         packedData, udpAddr = dsock.recvfrom(BUFFSIZE)
@@ -35,40 +34,84 @@ def main():
         sourcePort, destPort, seqNum, ackNum, headerLength, flags, receiveWindow, checksum, urgent, data = unpackData(packedData)
         FIN = flags >> 0
         ACK = flags >> 4
-
-        seqCheck = i * MSS
-        if (seqNum == seqCheck):
+        isSame = checkChecksum(sourcePort, destPort, data, checksum)
+        seqCheck = i
+        if (seqNum == seqCheck and isSame):
             print "Delivery completed successfully from %d" % udpAddr[1]
             outfile.write(data)
-
-            log = open(logFilename, "a")
-            timeNow = str(datetime.datetime.time(datetime.datetime.now()))
-            destIP = socket.gethostbyname(socket.gethostname())
-            messageToLog = timeNow + ", (" + senderIP + ":" + str(sourcePort) +\
-                           "), (" + destIP + ":" + str(destPort) + "), " +\
-                           str(seqNum) + ", " + str(ackNum) + ", ACK:" +\
-                           str(ACK) + " FIN:" + str(FIN) + "\n"
-            log.write(messageToLog)
-            log.close()
+            rcvTime = str(datetime.datetime.time(datetime.datetime.now()))
 
             if not tcpConnected:
                 tcpsock.connect((senderIP, senderPort))
-                msg = "ACK1"
+                msg = "ACK"
                 tcpsock.sendall(msg)
                 tcpConnected = True
             elif tcpConnected:
-                msg = "ACK2"
+                msg = "ACK"
                 tcpsock.sendall(msg)
-            i = i + 1
+
+            sentTime = str(datetime.datetime.time(datetime.datetime.now()))
+            rcvrIP = socket.gethostbyname(socket.gethostname())
+            writeLog(sentTime, rcvTime, senderIP, sourcePort, rcvrIP, destPort, seqNum, ackNum, ACK, FIN)
+
+            i += 1
             if (FIN == 1):
                 break
 
     outfile.close()
+    logFile.close()
+
+def getArgs():
+    # Assign command line args to variables and return
+    filename = sys.argv[1]
+    listeningPort = int(sys.argv[2])
+    senderIP = sys.argv[3]
+    senderPort = int(sys.argv[4])
+    logFilename = sys.argv[5]
+    return filename, listeningPort, senderIP, senderPort, logFilename
 
 def unpackData(packedData):
-    dataLen = len(packedData) - 20
-    s = struct.Struct("H H I I B B H H H " + str(dataLen) + "s")
+    dataLen = len(packedData) - HEADER_LEN
+    s = struct.Struct("H H I I B B H h H " + str(dataLen) + "s")
     sourcePort, destPort, seqNum, ackNum, headerLength, flags, receiveWindow, checksum, urgent, data = s.unpack(packedData)
     return sourcePort, destPort, seqNum, ackNum, headerLength, flags, receiveWindow, checksum, urgent, data
+
+def writeLog(sentTime, rcvTime, senderIP, senderPort, rcvrIP, rcvrPort, seqNum, ackNum, ACK, FIN):
+    global logFile
+    rcvPacketLog = "Received Packet:" + rcvTime + ", (" + senderIP + ":" + str(senderPort) +\
+               "), (" + rcvrIP + ":" + str(rcvrPort) + "), " +\
+               str(seqNum) + ", " + str(ackNum) + ", ACK:" +\
+               str(ACK) + " FIN:" + str(FIN) + "\n"
+    logFile.write(rcvPacketLog)
+    sentPacketLog = "Sent Packet:" + sentTime + ", (" + rcvrIP + ":" + str(rcvrPort) +\
+               "), (" + senderIP + ":" + str(senderPort) + "), " +\
+               str(seqNum) + ", " + str(ackNum + 1) + ", ACK:" +\
+               str(ACK) + " FIN:1" + "\n"
+    logFile.write(sentPacketLog)
+
+# Code taken from http://codewiki.wikispaces.com/ip_checksum.py.
+def calcChecksum(data):  # Form the standard IP-suite checksum
+    pos = len(data)
+    if (pos & 1):  # If odd...
+        pos -= 1
+        sum = ord(data[pos])  # Prime the sum with the odd end byte
+    else:
+        sum = 0
+
+    #Main code: loop to calculate the checksum
+    while pos > 0:
+        pos -= 2
+        sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+
+    result = (~ sum) & 0xffff #Keep lower 16 bits
+    result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+    return result
+
+def checkChecksum(sourcePort, destPort, data, checksum):
+    newChecksum = calcChecksum(data)
+    return newChecksum == checksum
 
 main()
--- sender.py
+++ sender.py
@@ -9,16 +9,18 @@ import threading
 import struct
 import os
 import math
-import time
 import datetime
 
 BUFFSIZE = 1024
 MSS = 576
 ALPHA = .125
 BETA = .25
+HEADER_LEN = 20
 
 retransmissionTimer = ""
 timeout = 1 # second
+logFile = ""
+retransCount = ""
 
 def main():
     global retransmissionTimer
@@ -52,19 +54,26 @@ def main():
 
     filesize = os.stat(filename).st_size
     infile = open(filename, "rb")
+    global logFile
+    logFile = open(logFilename, "w")
     j = 0
+    totalBytes = 0
+    global retransCount
+    retransCount = 0
     for i in range(0, filesize, MSS):
         dataChunk = infile.read(MSS)
-
-        seqNum = (j * MSS)
-        ackNum = (j * MSS)
+        totalBytes += len(dataChunk)
+        seqNum = j
+        ackNum = j
+        # If this segment is last segment, set FIN to 1
         if (j + 1) * MSS >= filesize:
             FIN = 1
         flags = FIN + (SYN << 1) + (RST << 2) + (PSH << 3) + (ACK << 4) + (URG << 5)
-        messageToSend = createHeader(ackPortNum, remotePort, seqNum, ackNum, 20, flags, 0, 0, 0, dataChunk)
-        isRetransmit = False
-        isRetransmit = transmit(dsock, messageToSend, remoteIP, remotePort, isRetransmit)
-        sentTime = datetime.datetime.today()
+
+        checksum = calcChecksum(dataChunk)
+        segmentToSend = createTCPSegment(ackPortNum, remotePort, seqNum, ackNum, HEADER_LEN, flags, 0, checksum, 0, dataChunk)
+
+        sentTime = transmit(dsock, segmentToSend, remoteIP, remotePort)
         rcvTime = ""
 
         if not tcpConnected:
@@ -74,7 +83,6 @@ def main():
             rcvTime = datetime.datetime.today()
             print "Received ACK:%s" % data
             tcpConnected = True
-
             retransmissionTimer.cancel()
         elif tcpConnected:
             data = tcpConn.recv(BUFFSIZE)
@@ -83,12 +91,20 @@ def main():
             retransmissionTimer.cancel()
 
         # Recalculate estimated RTT based on sample RTT
-        if not isRetransmit:
-            recalcRTT(sentTime, rcvTime, estimatedRTT, devRTT)
+        estimatedRTT = recalcRTT(sentTime, rcvTime, estimatedRTT, devRTT)
+
+        senderIP = socket.gethostbyname(socket.gethostname())
+        writeLog(sentTime, rcvTime, senderIP, ackPortNum, remoteIP, remotePort, seqNum, ackNum, ACK, FIN)
 
         j += 1
 
+    logFile.write("Estimated RTT:" + str(estimatedRTT) + " seconds")
     infile.close()
+    logFile.close()
+    retransCount -= j
+    print "Delivery completed successfully\nTotal bytes sent = " + str(totalBytes) + \
+          "\nSegments sent = " + str(j) + "\nSegments retransmitted = " + str(retransCount) + "\n"
+
 
 def getArgs():
     # Assign command line args to variables and return
@@ -100,18 +116,19 @@ def getArgs():
     windowSize = int(sys.argv[6])
     return filename, remoteIP, remotePort, ackPortNum, logFilename, windowSize
 
-def transmit(dsock, message, remoteIP, remotePort, isRetransmit):
+def transmit(dsock, data, remoteIP, remotePort):
     global retransmissionTimer
-    retransmissionTimer = threading.Timer(timeout, transmit, [dsock, message, remoteIP, remotePort, True])
+    global retransCount
+    retransmissionTimer = threading.Timer(timeout, transmit, [dsock, data, remoteIP, remotePort])
     retransmissionTimer.start()
-    print "Sending packet"
-    dsock.sendto(message, (remoteIP, remotePort))
-    return isRetransmit
+    dsock.sendto(data, (remoteIP, remotePort))
+    sentTime = datetime.datetime.today()
+    retransCount += 1
+    return sentTime
 
-
-def createHeader(sourcePort, destPort, seqNum, ackNum, headerLength, flags, receiveWindow, checksum, urgent, data):
+def createTCPSegment(sourcePort, destPort, seqNum, ackNum, headerLength, flags, receiveWindow, checksum, urgent, data):
     dataLen = len(data)
-    s = struct.Struct("H H I I B B H H H " + str(dataLen) + "s")
+    s = struct.Struct("H H I I B B H h H " + str(dataLen) + "s")
     values = (sourcePort, destPort, seqNum, ackNum, headerLength,
               flags, receiveWindow, checksum, urgent, data)
     packedHeader = s.pack(*values)
@@ -123,10 +140,43 @@ def recalcRTT(sentTime, rcvTime, estimatedRTT, devRTT):
     estimatedRTT = (1 - ALPHA) * estimatedRTT + (ALPHA * sampleRTT)
     devRTT = (1 - BETA) * devRTT + BETA * math.fabs(sampleRTT - estimatedRTT)
     timeout = estimatedRTT + (4 * devRTT)
-    print sampleRTT
-    print estimatedRTT
-    print devRTT
-    print timeout
+    return estimatedRTT
 
+def writeLog(sentTime, rcvTime, senderIP, ackPortNum, remoteIP, remotePort, seqNum, ackNum, ACK, FIN):
+    global logFile
+    timeSegmentSent = str(sentTime)
+    sentPacketLog = "Send Segment:" + timeSegmentSent + ", (" + senderIP + ":" + str(ackPortNum) +\
+               "), (" + remoteIP + ":" + str(remotePort) + "), " +\
+               str(seqNum) + ", " + str(ackNum) + ", ACK:" +\
+               str(ACK) + " FIN:" + str(FIN) + "\n"
+    logFile.write(sentPacketLog)
+    timeSegmentRcv = str(rcvTime)
+    rcvPacketLog = "Received Segment:" + timeSegmentRcv + ", (" + remoteIP + ":" + str(remotePort) +\
+               "), (" + senderIP + ":" + str(ackPortNum) + "), " +\
+               str(seqNum) + ", " + str(ackNum + 1) + ", ACK:" +\
+               str(ACK) + " FIN:1" + "\n"
+    logFile.write(rcvPacketLog)
+
+# Code taken from http://codewiki.wikispaces.com/ip_checksum.py.
+def calcChecksum(data):  # Form the standard IP-suite checksum
+    pos = len(data)
+    if (pos & 1):  # If odd...
+        pos -= 1
+        sum = ord(data[pos])  # Prime the sum with the odd end byte
+    else:
+        sum = 0
+
+    #Main code: loop to calculate the checksum
+    while pos > 0:
+        pos -= 2
+        sum += (ord(data[pos + 1]) << 8) + ord(data[pos])
+
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum += (sum >> 16)
+
+    result = (~ sum) & 0xffff #Keep lower 16 bits
+    result = result >> 8 | ((result & 0xff) << 8)  # Swap bytes
+    print result
+    return result
 
 main()
